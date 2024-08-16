5fe7fd1574e78c6d023b2d5f7208aa6b2c4cf7a9
Hugo Soares <hugosoares.dev@gmail.com>
2024-08-16 18:20:19 -0300

    prova de js01

---
+++ prova01.js
@@ -0,0 +1,31 @@
+
+let nota01 = Number(prompt('digite a primeitra nota: '));
+let nota02 = Number(prompt('digite a segunda nota: '));
+let nota03 = Number(prompt('digite a terceira nota: '));
+/*
+console.log(nota01);
+console.log(nota02);
+console.log(nota03);
+*/
+p01 = 2
+p02 = 2
+p03 = 4
+
+total_p = p01+p02+p03;
+total_n1 = nota01*p01;
+total_n2 = nota02*p02;
+total_n3 = nota03*p03;
+/*
+console.log(total_p);
+console.log(total_n1);
+console.log(total_n2);
+console.log(total_n3);
+*/
+alert('Resposta no console(F12)');
+console.log(`A media ponderada séra de ${(total_n1+total_n2+total_n3)/total_p}`)
+
+
+
+
+
+
